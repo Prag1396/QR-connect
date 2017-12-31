22d5c1f7e5764878852ccacbec9235c728b26f29
Pragun Sharma <pragunsharma@praguns-mbp.attlocal.net>
2017-12-31 03:38:59 -0800

    Added confirmvc

--- ComCard/Style/TextFieldStyle.swift
+++ ComCard/Style/TextFieldStyle.swift
@@ -13,12 +13,13 @@ class TextFieldStyle: UITextField {
     @IBInspectable var cornerRadius: CGFloat = 0.0 {
         didSet {
             self.layer.cornerRadius = cornerRadius
+            
         }
     }
     override func draw(_ rect: CGRect) {
         self.layer.masksToBounds = false
         self.layer.shadowRadius = 4.0
-        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowColor = UIColor.cyan.cgColor
         self.layer.shadowOffset = CGSize(width: 1, height: 1)
         self.layer.shadowOpacity = 0.5
     }
