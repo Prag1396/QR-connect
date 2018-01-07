c038aef0ed50e06d36c78747b79aad5e799ceb5a
Pragun Sharma <pragunsharma@praguns-mbp.attlocal.net>
2018-01-07 15:20:29 -0800

    Added pod for keyboard and textfield problem

--- ComCard/AppDelegate.swift
+++ ComCard/AppDelegate.swift
@@ -25,6 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 UIApplication.shared.registerForRemoteNotifications()
             })
         }
+        
         return true
     }
 
